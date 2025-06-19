6c1582a3938a0bc1a47926cc64620fe67d039355
oancius <stefan.oancea93@gmail.com>
2025-06-19 19:38:02 +0300

    Fix logo url

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -2,7 +2,7 @@ import xapoLogo from "../../assets/logo.svg";
 
 function Logo() {
   return (
-    <a href={"/"}>
+    <a href={"https://www.xapobank.com/en"}>
       <img src={xapoLogo} alt={"xapoLogo"} />
     </a>
   );
